18a751a1cf851d8d7a7dfa86223a82b831a7b3d9
licorice-ndd <nguyenducduy0706@gmail.com>
2023-03-03 11:25:09 +0700

    query

--- CRUDNet5/Form1.cs
+++ CRUDNet5/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,7 @@ namespace CRUDNet5
             InitializeComponent();
         }
 
+        string connectionString = @"Data Source=DESKTOP-CLB5LQN;Initial Catalog=PE_Demo_S2019;Integrated Security=True";
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -26,7 +28,11 @@ namespace CRUDNet5
 
         private void btnView_Click(object sender, EventArgs e)
         {
-
+            string sql = "Select * from Customer";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
         }
 
         private void btbAdd_Click(object sender, EventArgs e)
