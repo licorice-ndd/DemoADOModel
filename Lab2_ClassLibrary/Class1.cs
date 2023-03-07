1b79f9d0a919fa3229cb1af51e16ac175442e794
licorice-ndd <nguyenducduy0706@gmail.com>
2023-03-07 09:57:56 +0700

    start lab 2

---
+++ Lab2_ClassLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab2_ClassLibrary
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Lab2_WinForm/Form1.cs
@@ -0,0 +1,10 @@
+namespace Lab2_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
